82d1203bf6a06586ce75872c2eaae6cce6fdba22
jeffilvu <jeffersonaraujodeb@gmail.com>
2025-02-20 16:36:47 -0300

    Adicionando exercicios

--- ex3.c
+++ ex3.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
+
+/* 
+
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of  to their sum, and  to their absolute difference. There is no return value, and no return statement is needed.
+
+*/
 void update(int *a,int *b) {
     
     
     int aTemp = *a + *b;
-    int bTemp = *b - *a;
+    int bTemp = abs(*b - *a);
     
     *a = aTemp;
     *b = bTemp;
